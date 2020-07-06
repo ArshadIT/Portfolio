6bed4720ff458b6dd9cbbaa5241252fefac8aa0f
ArshadIT <arshadecon@gmail.com>
2020-07-06 15:09:21 +0200

    profile pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './app/profile/profile.component';
+import { AboutComponent } from './app/profile/about/about.component';
+import { ContactComponent } from './app/profile/contact/contact.component';
+import { EducationComponent } from './app/profile/education/education.component';
+import { ExperienceComponent } from './app/profile/experience/experience.component';
+import { FooterComponent } from './app/profile/footer/footer.component';
+import { HeaderComponent } from './app/profile/header/header.component';
+import { IntroComponent } from './app/profile/intro/intro.component';
+import { ReferenceComponent } from './app/profile/reference/reference.component';
+import { SkillsComponent } from './app/profile/skills/skills.component';
+import { PortfolioComponent } from './app/profile/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    AboutComponent,
+    ContactComponent,
+    EducationComponent,
+    ExperienceComponent,
+    FooterComponent,
+    HeaderComponent,
+    IntroComponent,
+    ReferenceComponent,
+    SkillsComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
